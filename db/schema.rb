c56c5b1ebce06b1df54733a8dd2e88583190d3ee
root <leomarkes21@gmail.com>
2019-02-18 20:43:45 -0300

    add association betewwn user and articles and debugger view

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_224634) do
+ActiveRecord::Schema.define(version: 2019_02_18_232056) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
